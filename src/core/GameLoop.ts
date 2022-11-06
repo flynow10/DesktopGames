a11708b58a15e80f8688e06069cf2871a4c1a2db
Nate Wagner <nate.o.wagner@gmail.com>
2022-11-06 12:43:57 -0500

    Fixed update loop and remove unused file

--- src/core/GameLoop.ts
+++ src/core/GameLoop.ts
@@ -3,6 +3,7 @@ export class GameLoop {
   public onFixedLoop: { (dt: number): void }[] = [];
   private lastUpdate: number = 0;
   private lastFixedUpdate: number = 0;
+  public fixedUpdateStep: number = 50;
   private _startTime: number = -1;
   private _stop: boolean = false;
 
@@ -16,16 +17,22 @@ export class GameLoop {
     this._stop = true;
   }
   private loop(time: number): void {
+    if (this._startTime === -1) {
+      this._startTime = time;
+    }
+    const totalElapsed = time - this._startTime;
+    this.onLoop.forEach((method) => method(totalElapsed - this.lastUpdate));
+    if (this.lastFixedUpdate + this.fixedUpdateStep <= totalElapsed) {
+      this.onFixedLoop.forEach((method) =>
+        method(totalElapsed - this.lastFixedUpdate)
+      );
+      this.lastFixedUpdate = totalElapsed;
+    }
+    this.lastUpdate = totalElapsed;
     if (!this._stop) {
       window.requestAnimationFrame((time) => {
         this.loop(time);
       });
     }
-    if (this._startTime === -1) {
-      this._startTime = time;
-    }
-    const elapsed = time - this._startTime;
-    this.onLoop.forEach((method) => method(elapsed - this.lastUpdate));
-    this.lastUpdate = elapsed;
   }
 }
