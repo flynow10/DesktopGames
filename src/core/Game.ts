1752541d79ed278b16248dbcf151029e94185694
Nate Wagner <nate.o.wagner@gmail.com>
2022-11-10 20:17:58 -0500

    Fix game pause and add game options

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -8,15 +8,18 @@ export abstract class Game {
   };
   public readonly gameLoop: GameLoop = new GameLoop();
   protected get paused(): boolean {
-    return this._paused || this._isTabVisible;
+    return this._paused || !this._isTabVisible;
   }
   protected set paused(value: boolean) {
     this._paused = value;
   }
   private _paused: boolean = false;
   private _isTabVisible: boolean = false;
+  private _gameOptions: GameOptions;
 
-  public constructor() {
+  public constructor(options: Partial<GameOptions> = {}) {
+    this._gameOptions = Object.assign({}, defaultOptions, options);
+    this.gameLoop.fixedUpdateStep = this._gameOptions.fixedUpdateStep;
     this.gameLoop.onLoop.push(this.internalUpdate.bind(this));
     this.gameLoop.onFixedLoop.push(this.fixedUpdate.bind(this));
   }
@@ -27,20 +30,30 @@ export abstract class Game {
   public selected(isSelected: boolean) {
     this._isTabVisible = isSelected;
   }
-  public abstract attach(node: any): void;
   private internalUpdate(dt: number) {
-    this.update(dt);
-    if (this._isTabVisible) {
+    if (!this.paused) {
+      this.update(dt);
+    }
+    if (
+      this._isTabVisible &&
+      (this._gameOptions.drawWhenPaused || !this.paused)
+    ) {
       this.draw(dt);
     }
   }
-  public abstract draw(dt: number): void;
-  public abstract update(dt: number): void;
-  public fixedUpdate(dt: number): void {}
+  public internalFixedUpdate(dt: number): void {
+    if (!this.paused) {
+      this.fixedUpdate(dt);
+    }
+  }
   public internalCleanup(): void {
     this.cleanup();
     this.gameLoop.stop();
   }
+  public abstract attach(node: any): void;
+  public abstract draw(dt: number): void;
+  public abstract update(dt: number): void;
+  public fixedUpdate(dt: number): void {}
   public cleanup(): void {}
 }
 
@@ -62,3 +75,13 @@ export enum GameCatagories {
   Puzzle,
   Skill,
 }
+
+export type GameOptions = {
+  fixedUpdateStep: number;
+  drawWhenPaused: boolean;
+};
+
+const defaultOptions: GameOptions = {
+  fixedUpdateStep: (1 / 60) * 1000,
+  drawWhenPaused: false,
+};
