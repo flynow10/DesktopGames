ebb00169cfd537a2d2bc63453330f915287277c7
Nate Wagner <nate.o.wagner@gmail.com>
2022-12-10 16:04:25 -0500

    Add button to theme manager

--- src/renderer/ThemeManager.tsx
+++ src/renderer/ThemeManager.tsx
@@ -1,15 +1,31 @@
 import { ReactNode, useState } from "react";
+import { Sun, Moon } from "lucide-react";
 
 export interface ThemeManagerProps {
   children: ReactNode
 }
 
+interface ThemeSwitchButtonProps {
+  isDark: boolean,
+  onClick: () => void
+}
+function ThemeSwitchButton(props: ThemeSwitchButtonProps) {
+    return (
+      <button onClick={props.onClick} className="theme-icon">
+        {props.isDark ? (<Moon/>) : (<Sun/>)}
+      </button>
+    )
+}
+
 export default function ThemeManager(props: ThemeManagerProps) {
   const [dark, setDark] = useState(false);
 
+  const themeClass = (dark ? "theme--dark" : "theme--light");
+
   return (
-    <div className={dark ? "theme--dark" : "theme--light"}>
+    <div className={"theme-manager " + themeClass}>
       {props.children}
+      <ThemeSwitchButton isDark={dark} onClick={() => setDark(!dark)}/>
     </div>
   )
 }
